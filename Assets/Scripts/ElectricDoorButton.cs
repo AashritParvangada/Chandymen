9172abc1629a23f3860a4418459a7442c1d64176
AashritParvangada <aashrit.parvangada@gmail.com>
2019-05-11 16:17:46 +0530

    Doors Set up

---
+++ Assets/Scripts/ElectricDoorButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricDoorButton : MonoBehaviour
+{
+
+    [SerializeField] GameObject G_Door;
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            OpenDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        G_Door.SetActive(false);
+    }
+}
