07c48fc0fd343ae30ad42e88faaf47738a25bb61
AashritParvangada <aashrit.parvangada@gmail.com>
2019-05-12 19:01:55 +0530

    More code commenting

--- Assets/Scripts/PlasmaBullet.cs
+++ Assets/Scripts/PlasmaBullet.cs
@@ -29,7 +29,7 @@ public class PlasmaBullet : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, maxStepDistance))
         {
-            //Debug.Log("Hit something");
+            //Calculates the angle for reflecting the bullet from walls.
             Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
             float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(0, rot, 0);
