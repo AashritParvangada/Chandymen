4aabc4a57735857763e7fb8f857320783cf54981
AashritParvangada <aashrit.parvangada@gmail.com>
2019-05-28 18:42:08 +0530

    Token 1 Whitebox First Draft

---
+++ Assets/Scripts/AcidSafeSpace.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidSafeSpace : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            other.GetComponent<PlayerController>().b_AboveAcid = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            other.GetComponent<PlayerController>().b_AboveAcid = false;
+        }
+    }
+}
