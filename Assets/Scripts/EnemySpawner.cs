8db26c021168dad6564d75192a48f357e856017a
AashritParvangada <aashrit.parvangada@gmail.com>
2019-06-10 20:21:37 +0530

    Work on Token 2.2

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject GO_EnemyToSpawn;
+
+    public void SpawnEnemy()
+    {
+        Instantiate(GO_EnemyToSpawn, transform);
+    }
+}
---
+++ Assets/Scripts/SpawnTrigger.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    bool b_isActive = false;
+    int i_numberOfSpawnersWithin = 0;
+    int i_enemiesKilled = 0;
+    [SerializeField] GameObject GO_DoorToOpen;
+
+    private void Start()
+    {
+        CountNumberOfSpawners();
+    }
+    private void OnEnable()
+    {
+        EventManager.OnEnemyKilled += KilledEnemy;
+
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnEnemyKilled -= KilledEnemy;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>() && !b_isActive)
+        {
+            SpawnEnemies();
+            SetActive();
+            GetComponent<BoxCollider>().enabled = false;
+
+        }
+    }
+
+    void SpawnEnemies()
+    {
+        foreach (EnemySpawner _EnSpawn in GetComponentsInChildren<EnemySpawner>())
+        {
+            _EnSpawn.SpawnEnemy();
+        }
+    }
+
+    void SetActive()
+    {
+        b_isActive = true;
+    }
+
+    void CountNumberOfSpawners()
+    {
+        foreach (EnemySpawner _EnSpawn in GetComponentsInChildren<EnemySpawner>())
+        {
+            i_numberOfSpawnersWithin++;
+        }
+    }
+
+    void KilledEnemy()
+    {
+        if (b_isActive)
+        {
+            i_enemiesKilled++;
+            if (i_enemiesKilled >= i_numberOfSpawnersWithin)
+            {
+                OpenDoor();
+            }
+        }
+    }
+
+    void OpenDoor()
+    {
+        GO_DoorToOpen.SetActive(false);
+    }
+
+
+}
