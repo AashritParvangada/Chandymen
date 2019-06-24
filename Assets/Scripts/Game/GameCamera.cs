af93dccc99d64c30fd4f81842bd4d30053ebdd89
AashritParvangada <aashrit.parvangada@gmail.com>
2019-06-24 18:43:51 +0530

    Added puzzle cameras to Level 3

--- Assets/Scripts/Game/GameCamera.cs
+++ Assets/Scripts/Game/GameCamera.cs
@@ -6,17 +6,14 @@ public class GameCamera : MonoBehaviour
 {
     Transform trans_target;
     public Vector3 v3_distanceFromPlayer, v3_viewingAngle;
+    [SerializeField] float F_camSize;
     Vector3 v3_camTarget;
     public bool B_PuzzleMode;
     // Use this for initialization
     void Start()
     {
-        if (!B_PuzzleMode)
-        {
-
-            trans_target = GameObject.FindObjectOfType<PlayerController>().transform;
-            transform.eulerAngles = new Vector3(80, 0, 0);
-        }
+        trans_target = GameObject.FindObjectOfType<PlayerController>().transform;
+        F_camSize = GetComponent<Camera>().orthographicSize;
     }
 
     // Update is called once per frame
@@ -38,5 +35,20 @@ public class GameCamera : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, v3_camTarget, Time.deltaTime * 8);//Lerp smoothly to cam target.
 
         transform.eulerAngles = v3_viewingAngle;//Look at player from this specific angle.
+    }
+
+    public void EnterPuzzleMode(Transform _newCamPos, float _camSize)
+    {
+        B_PuzzleMode = true;
+        transform.SetParent(_newCamPos);
+        transform.localPosition = new Vector3(0, 0, 0);
+        GetComponent<Camera>().orthographicSize = _camSize;
+    }
+
+    public void ExitPuzzleMode()
+    {
+        B_PuzzleMode = false;
+        transform.SetParent(null);
+        GetComponent<Camera>().orthographicSize = F_camSize;
     }
 }
---
+++ Assets/Scripts/Level/PuzzleCamLock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleCamLock : MonoBehaviour
+{
+    [SerializeField] Transform Trans_camPos;
+    [SerializeField] float F_camSize;
+
+    Camera GO_PlayerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GO_PlayerCamera = FindObjectOfType<Camera>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            GO_PlayerCamera.GetComponent<GameCamera>().EnterPuzzleMode(Trans_camPos, F_camSize);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            GO_PlayerCamera.GetComponent<GameCamera>().B_PuzzleMode = false;
+            GO_PlayerCamera.transform.SetParent(null);
+        }
+    }
+}
