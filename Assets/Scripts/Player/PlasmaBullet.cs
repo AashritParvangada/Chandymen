bfd245d00009b332b0a252a6c3d87eea42b08108
AashritParvangada <aashrit.parvangada@gmail.com>
2019-06-27 06:39:46 +0530

    Plasma Bullets Fixed

--- Assets/Scripts/Player/PlasmaBullet.cs
+++ Assets/Scripts/Player/PlasmaBullet.cs
@@ -10,6 +10,7 @@ public class PlasmaBullet : MonoBehaviour
     public int I_BulletCharge = 0;
     [SerializeField] float F_bulletDestroyTime = 10;
     Rigidbody rb_rb;
+    public Vector3 V3_Dir;
     private void Start()
     {
         rb_rb = GetComponent<Rigidbody>();
@@ -20,7 +21,7 @@ public class PlasmaBullet : MonoBehaviour
     {
         if (other.tag == "Reflector")
         {
-            Reflect();
+            Reflect(V3_Dir);
         }
 
         else if (other.GetComponent<Grunt>())
@@ -35,21 +36,11 @@ public class PlasmaBullet : MonoBehaviour
         }
     }
 
-    void Reflect()//Ray that reflects the bullet. Must be placed in separate object.
+    void Reflect(Vector3 _dir)//Ray that reflects the bullet. Must be placed in separate object.
     {
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, F_MaxStepDistance))
-        {
-            //Debug.Log("Hit Reflector");
-            //Calculates the angle for reflecting the bullet from walls.
-            Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
-
-            float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0, rot, 0);
-            rb_rb.velocity = 10 * transform.forward;
-            I_BulletCharge++;
-        }
+        transform.forward = _dir;
+        rb_rb.velocity = 10 * transform.forward;
+        I_BulletCharge++;
     }
 
     IEnumerator DestroyBullet()
---
+++ Assets/Scripts/Player/PlasmaBulletRayDetect.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PlasmaBulletRayDetect : MonoBehaviour
+{
+    PlasmaBullet PlasBull_ParentBullet;
+
+    private void Start()
+    {
+        PlasBull_ParentBullet = GetComponentInParent<PlasmaBullet>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Reflector")
+        {
+            CalculateReflectionAngle();
+        }
+    }
+
+    void CalculateReflectionAngle()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            //Debug.Log("Hit Reflector");
+            //Calculates the angle for reflecting the bullet from walls.
+            Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
+
+            float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
+            SetPlasBullRot(reflectDirection);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * 100);
+
+            //Debug.Log("Hit Reflector");
+            //Calculates the angle for reflecting the bullet from walls.
+            Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
+            Gizmos.DrawLine(hit.point, hit.point + reflectDirection * 100);
+        }
+    }
+
+
+    void SetPlasBullRot(Vector3 _dir)
+    {
+        PlasBull_ParentBullet.V3_Dir = _dir;
+    }
+}
