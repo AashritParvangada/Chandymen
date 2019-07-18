a545c9ef9fdb67dd7e40dca57eadad65075e2cbf
AashritParvangada <aashrit.parvangada@gmail.com>
2019-07-18 07:21:50 +0530

    Main Menu Setting Graphics Quality and Volume

---
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer AudMix_Mixer;
+    public void SetVolume(float _volume)
+    {
+        AudMix_Mixer.SetFloat("Volume", _volume);
+    }
+
+    public void SetQuality(int _qualityIndex)
+    {
+        Debug.Log("Should set Quality to: " + _qualityIndex); Debug.Log("Quality was: " + QualitySettings.GetQualityLevel());
+        QualitySettings.SetQualityLevel(_qualityIndex);
+
+        Debug.Log("Quality is now: " + QualitySettings.GetQualityLevel());
+    }
+}
