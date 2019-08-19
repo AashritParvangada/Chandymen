2222d0bd9490aa0d3dff1cee41f6f77d2b7a58e5
Vrushank Gajjar <vrushplay@gmail.com>
2019-08-19 21:19:59 +0530

    Revert "Holding Buttons Down"
    This reverts commit 0887a21874363ed708590b7b5ceb902e5f3a7c55.

--- Assets/Scripts/Level/ElectricDoorButton.cs
+++ Assets/Scripts/Level/ElectricDoorButton.cs
@@ -6,21 +6,12 @@ using UnityEngine.AI;
 public class ElectricDoorButton : MonoBehaviour
 {
     [SerializeField] GameObject G_door;
-    [SerializeField] float F_holdTime;
-    float f_startTime; bool b_newKey = false;
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKeyDown(KeyCode.E))
         {
-            f_startTime = Time.time;
-            b_newKey = true;
+            OpenDoor();
         }
-        if ((Input.GetKey(KeyCode.Joystick1Button1) || Input.GetKey(KeyCode.E)) && b_newKey)
-        {
-            Debug.Log(Time.time - f_startTime);
-            if (Time.time - f_startTime >= F_holdTime) OpenDoor();
-        }
-        if (Input.GetKeyUp(KeyCode.Joystick1Button1) || Input.GetKeyUp(KeyCode.E)) b_newKey = false;
     }
 
     //Opens the door and rebakes the nav mesh.
