d072aa4364a68b1d70b51eb0a2454b54679cb07d
AashritParvangada <aashrit.parvangada@gmail.com>
2019-11-17 20:30:21 +0530

    Credits Names Integration WIP

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] Animator Anim_flare, Anim_aashrit, Anim_vrushank, Anim_davin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayCredits());
+    }
+
+    void RunFlare()
+    {
+        Anim_flare.SetTrigger("Flare_Sweep");
+    }
+
+    void RunName(Animator _name)
+    {
+        _name.SetTrigger("Name_Sweep");
+    }
+
+    IEnumerator PlayCredits()
+    {
+        yield return new WaitForSeconds(1);
+        RunFlare();
+        RunName(Anim_aashrit);
+    }
+
+}
