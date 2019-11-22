b66116e8a665d5109a21bb9246a0ac1f314d4c67
AashritParvangada <aashrit.parvangada@gmail.com>
2019-11-22 07:31:36 +0530

    Added Tutorial Script

---
+++ Assets/Scripts/Level/PuzzleTutorial.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTutorial : MonoBehaviour
+{
+    [SerializeField] GameObject Go_panel;
+    bool b_canInteract = false;
+    bool b_panelActive = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            b_canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            b_canInteract = false;
+            Go_panel.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        CheckPlayerInput();
+    }
+
+    void CheckPlayerInput()
+    {
+        if (b_canInteract && !b_panelActive && (Input.GetKeyDown(KeyCode.JoystickButton1)
+ || Input.GetKeyDown(KeyCode.E)))
+        {
+            SetPanelActive(true);
+        }
+
+        if (b_canInteract && b_panelActive && (Input.GetKeyDown(KeyCode.JoystickButton1)
+|| Input.GetKeyDown(KeyCode.E)))
+        {
+            SetPanelActive(false);
+        }
+    }
+
+    void SetPanelActive(bool _active)
+    {
+        StartCoroutine(IEnum_InteractCooldown());
+        Go_panel.SetActive(_active);
+        b_panelActive = _active;
+    }
+
+    IEnumerator IEnum_InteractCooldown()
+    {
+        b_canInteract = false;
+        yield return new WaitForSeconds(1);
+        b_canInteract = true;
+    }
+}
