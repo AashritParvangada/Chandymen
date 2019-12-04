021b8856ca3fb5ced89a04d31abc22b95e6fbb68
AashritParvangada <aashrit.parvangada@gmail.com>
2019-12-04 07:24:27 +0530

    Added Credits

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
-    [SerializeField] Animator Anim_flare, Anim_aashrit, Anim_vrushank, Anim_davin;
+    [SerializeField] Animator Anim_flare, Anim_aashrit, Anim_vrushank, Anim_davin, Anim_logo;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,11 @@ public class Credits : MonoBehaviour
         Anim_flare.SetTrigger("Flare_Sweep");
     }
 
+    void LogoBang()
+    {
+        Anim_logo.SetTrigger("Logo_Bang");
+    }
+
     void RunName(Animator _name)
     {
         _name.SetTrigger("Name_Sweep");
@@ -24,7 +29,8 @@ public class Credits : MonoBehaviour
 
     IEnumerator PlayCredits()
     {
-        yield return new WaitForSeconds(1);
+        LogoBang();
+        yield return new WaitForSeconds(7);
         RunFlare();
         RunName(Anim_aashrit);
         yield return new WaitForSeconds(5);
