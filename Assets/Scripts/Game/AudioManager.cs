41db6901e3aee78488328cad007de2914fa34313
AashritParvangada <aashrit.parvangada@gmail.com>
2020-01-08 07:46:57 +0530

    Implemented Main Game Music

---
+++ Assets/Scripts/Game/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] bool B_isMainMenu = false;
+
+    [SerializeField] AudioClip AudClp_mainMenu;
+    [SerializeField] AudioClip AudClp_combat;
+    [SerializeField] AudioClip AudClp_puzzle;
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source=GetComponent<AudioSource>();
+    }
+
+    public void SetPiecePuzzle(bool isPuzzle)
+    {
+        if(isPuzzle) source.clip=AudClp_puzzle;
+        else source.clip=AudClp_combat;
+        source.Play();
+    }
+
+}
--- Assets/Scripts/Level/Checkpoint.cs
+++ Assets/Scripts/Level/Checkpoint.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField] bool B_isPuzzle = false;
     //Just sends a command to game manager.
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>())
+        {
             FindObjectOfType<GameManager>().SetLastCheckpoint(this);
+            FindObjectOfType<AudioManager>().SetPiecePuzzle(B_isPuzzle);
+        }
     }
 }
