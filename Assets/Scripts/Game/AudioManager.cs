5af3e910d25a32398bbefa661701448700a18690
AashritParvangada <aashrit.parvangada@gmail.com>
2020-01-09 07:26:15 +0530

    Music Fading In and Out

--- Assets/Scripts/Game/AudioManager.cs
+++ Assets/Scripts/Game/AudioManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] bool B_isMainMenu = false;
-
+    [SerializeField] float F_totalFadeTime, F_sourceMaxVolume;
     [SerializeField] AudioClip AudClp_mainMenu;
     [SerializeField] AudioClip AudClp_combat;
     [SerializeField] AudioClip AudClp_puzzle;
@@ -14,14 +14,35 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        source=GetComponent<AudioSource>();
+        source = GetComponent<AudioSource>();
     }
 
     public void SetPiecePuzzle(bool isPuzzle)
     {
-        if(isPuzzle) source.clip=AudClp_puzzle;
-        else source.clip=AudClp_combat;
-        source.Play();
+        StartCoroutine(IEnumFadeAudioToAnotherPiece(isPuzzle, F_totalFadeTime));
     }
 
+    IEnumerator IEnumFadeAudioToAnotherPiece(bool _isPuzzle, float _totalTime)
+    {
+        float _currentTime = 0;
+
+        while (source.volume > 0)
+        {
+            _currentTime += Time.deltaTime;
+            source.volume = Mathf.Lerp(F_sourceMaxVolume, 0, _currentTime / (_totalTime / 2));
+            yield return null;
+        }
+
+        if (_isPuzzle) source.clip = AudClp_puzzle;
+        else source.clip = AudClp_combat;
+
+        source.Play();
+
+        while (source.volume < F_sourceMaxVolume)
+        {
+            _currentTime += Time.deltaTime;
+            source.volume = Mathf.Lerp(0, F_sourceMaxVolume, _currentTime / (_totalTime / 2));
+            yield return null;
+        }
+    }
 }
--- Assets/Scripts/Level/Checkpoint.cs
+++ Assets/Scripts/Level/Checkpoint.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] bool B_isPuzzle = false;
+    bool b_enteredYet = false;
+    [SerializeField] ElectricDoor elecdoor_ToClose;
     //Just sends a command to game manager.
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>())
         {
             FindObjectOfType<GameManager>().SetLastCheckpoint(this);
-            FindObjectOfType<AudioManager>().SetPiecePuzzle(B_isPuzzle);
+
+            if (!b_enteredYet)
+            {
+                FindObjectOfType<AudioManager>().SetPiecePuzzle(B_isPuzzle);
+                b_enteredYet = true;
+
+                if (elecdoor_ToClose) elecdoor_ToClose.SwitchDoor(true);
+            }
         }
     }
 }
