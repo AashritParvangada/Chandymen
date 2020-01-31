3fb015e991d13b6110af9c3ef8b963c9f2e3baff
AashritParvangada <aashrit.parvangada@gmail.com>
2020-01-31 07:12:39 +0530

    Added Kill Volumes to the Game

---
+++ Assets/Scripts/Level/KillVolume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillVolume : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if(other.GetComponent<PlayerController>())
+        {
+            other.GetComponent<PlayerController>().DamageHealth(100);
+        }
+    }
+}
